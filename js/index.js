8da96a3d4366e1a8619f58a07a1e3d2be0cb23d9
kinya1738 <shirleen.njeru@student.moringaschool.com>
2025-03-26 14:02:14 -0700

    initial js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-const base_url = 'http://localhost:3000/wigs'
+const base_url = 'http://localhost:4000/wigs'
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(base_url) 
+        .then(response => response.json())
+        .then(data => {
+            const wigContainer = document.getElementById("wig-images"); 
+
+            data.forEach(wig => {
+                const wigImages = document.createElement("div");
+    
+             wigImages.innerHTML = `
+                    <img src="${wig.image}" alt="${wig.title}">
+                    <h3>${wig.title}</h3>
+                    <p>${wig.description}</p>
+                    <div>${wig.price}</div>
+                `;
+
+                wigContainer.appendChild(wigImages);
+            });
+        })
+        .catch(error => console.error("Error fetching wigs:", error));
+});
+
